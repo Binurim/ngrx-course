7c8373f9a1ec7e0ebe4acb687db52d44fb75e026
binuri <binurim95@gmail.com>
2024-04-14 21:21:43 +0530

    Implemented user logout

--- src/app/auth/reducers/index.ts
+++ src/app/auth/reducers/index.ts
@@ -30,7 +30,12 @@ export const authReducer = createReducer(
     return {
       user: action.user
     }
-  })
+  }),
 
+  on(AuthActions.logout, (state, action) => {
+    return {
+      user: undefined
+    }
+  })
 
 );
