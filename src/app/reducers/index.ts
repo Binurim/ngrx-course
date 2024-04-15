78173582fbf55e81aca8e21b51fe21bb10b593fc
binuri <binurim95@gmail.com>
2024-04-15 22:58:46 +0530

    Setting up NgRx Router Store and the time travelling debugger

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -5,10 +5,15 @@ import {
     createSelector,
     MetaReducer
   } from '@ngrx/store';
+import { environment } from '../../environments/environment';
+import { routerReducer } from '@ngrx/router-store';
 
   export interface AppState {
   }
   
   export const reducers: ActionReducerMap<AppState> = {
+    router: routerReducer
   };
+
+  export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [] : [];
   
