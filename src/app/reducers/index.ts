53d2991643cf29454df4d66f03a01a511f75ea52
binuri <binurim95@gmail.com>
2024-04-16 17:20:20 +0530

    Added NgRx metareducers

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -15,5 +15,14 @@ import { routerReducer } from '@ngrx/router-store';
     router: routerReducer
   };
 
-  export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [] : [];
+  export function logger(reducer: ActionReducer<any>): ActionReducer<any> {
+    return (state, action) => {
+      console.log("state before: ", state);
+      console.log("action ", action);
+
+      return reducer(state, action);
+    }
+  }
+
+  export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [logger] : [];
   
